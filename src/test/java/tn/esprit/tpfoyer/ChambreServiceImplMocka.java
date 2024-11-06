f35bdd61b10ca72556e80b971bcc1fa536f4b007
GhaithDhaouadi <ghaith.dhaouadi@esprit.tn>
2024-11-06 19:54:03 +0100

    tesssttt

--- src/test/java/tn/esprit/tpfoyer/ChambreServiceImplMocka.java
+++ src/test/java/tn/esprit/tpfoyer/ChambreServiceImplMocka.java
@@ -1,26 +1,42 @@
 package tn.esprit.tpfoyer;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import tn.esprit.tpfoyer.entity.Chambre;
+import tn.esprit.tpfoyer.service.ChambreServiceImpl;
 import tn.esprit.tpfoyer.service.IChambreService;
 
+
+import java.util.Collections;
 import java.util.List;
 
-@SpringBootTest
-@TestMethodOrder(OrderAnnotation.class)
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
+@TestMethodOrder(OrderAnnotation.class)
 public class ChambreServiceImplMocka {
-    @Autowired
+
+    @Mock
     IChambreService us;
+
+    @InjectMocks
+    ChambreServiceImpl chambreServiceImpl;
+
     @Test
     @Order(1)
-    public void testRetrieveAllUsers() {
-        List<Chambre> listUsers = us.retrieveAllChambres();
-        Assertions.assertEquals(0, listUsers.size());
+    public void testRetrieveAllChambres() {
+        List<Chambre> mockChambres = Collections.emptyList();
+        when(us.retrieveAllChambres()).thenReturn(mockChambres);
+
+        List<Chambre> listChambres = chambreServiceImpl.retrieveAllChambres();
+
+        Assertions.assertEquals(0, listChambres.size());
     }
 }
